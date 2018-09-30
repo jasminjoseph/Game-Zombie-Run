ab13bf573011ea2a3fae0e1cfb7165bab7b1b224
Jasmin John Joseph <jasminjohnjoseph1@gmail.com>
2018-09-30 15:17:30 -0500

    Platform code

---
+++ Scripts/platformCode.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformCode : MonoBehaviour {
+
+	[SerializeField] private float objSpeed = 1f;	
+	[SerializeField] private float resetPos = -28.62f;
+	[SerializeField] private float lastPos = 66.4f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () {
+		transform.Translate(Vector3.left * (objSpeed * Time.deltaTime));
+		if (transform.localPosition.x <= resetPos) {
+			Vector3 newPos = new Vector3 (lastPos, transform.localPosition.y, transform.position.z);
+			transform.position = newPos;
+		}
+
+	}  
+}
